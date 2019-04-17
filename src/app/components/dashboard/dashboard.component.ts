afe163227bc9b396725eca42b37950aa22d47ec3
disney50 <36032319+disney50@users.noreply.github.com>
2019-04-17 11:39:38 +0200

    players buttons dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -15,26 +15,107 @@ import * as componentActions from '../../store/actions/component.actions';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  playersClicked = false;
   inventoryClicked = false;
-  typesClicked = false;
+  rentalsClicked = false;
+
+  u19Clicked = false;
+  u15Clicked = false;
+  u13Clicked = false;
+  // u10Clicked = navigate;
+  // beginnersClicked = navigate;
+  // playersAddClicked = navigate;
+
+  ladiesClicked = false;
+  menClicked = false;
+
+  inventoryTypeClicked = false;
+  inventoryStatusClicked = false;
+  inventoryAddClicked = false;
+
+  rentalsTypeClicked = false;
+  rentalsStatusClicked = false;
+  rentalsAddClicked = false;
 
   constructor(
     private router: Router,
     private store: Store<AppState>,
   ) { }
 
-  inventoryToggle() {
-    this.inventoryClicked = !this.inventoryClicked;
+  toggleU19Clicked() {
+    if (this.u19Clicked) {
+      this.u19Clicked = false;
+    } else {
+      this.u19Clicked = true;
+      this.u15Clicked = false;
+      this.u13Clicked = false;
+    }
   }
 
-  typesToggle() {
-    this.typesClicked = !this.typesClicked;
+  toggleU15Clicked() {
+    if (this.u15Clicked) {
+      this.u15Clicked = false;
+    } else {
+      this.u15Clicked = true;
+      this.u19Clicked = false;
+      this.u13Clicked = false;
+    }
   }
 
-  navigateInventory(selectedType: string) {
-    this.store.dispatch(new componentActions.SelectType(selectedType));
-    this.router.navigateByUrl('inventory');
+  toggleU13Clicked() {
+    if (this.u13Clicked) {
+      this.u13Clicked = false;
+    } else {
+      this.u13Clicked = true;
+      this.u19Clicked = false;
+      this.u15Clicked = false;
+    }
   }
+
+  navigateList() {
+    console.log('navigateList');
+
+  }
+
+  navigateAdd() {
+    console.log('navigateAdd');
+
+  }
+
+  togglePlayersClicked() {
+    if (this.playersClicked) {
+      this.playersClicked = false;
+    } else {
+      this.playersClicked = true;
+      this.inventoryClicked = false;
+      this.rentalsClicked = false;
+    }
+  }
+
+  toggleInventoryClicked() {
+    if (this.inventoryClicked) {
+      this.inventoryClicked = false;
+    } else {
+      this.inventoryClicked = true;
+      this.playersClicked = false;
+      this.rentalsClicked = false;
+    }
+  }
+
+  toggleRentalsClicked() {
+    if (this.rentalsClicked) {
+      this.rentalsClicked = false;
+    } else {
+      this.rentalsClicked = true;
+      this.playersClicked = false;
+      this.inventoryClicked = false;
+    }
+  }
+
+  // navigateInventory(selectedType: string) {
+  //   this.store.dispatch(new componentActions.SelectType(selectedType));
+  //   this.router.navigateByUrl('inventory');
+  // }
 
   sliceAppState() {
     this.store.dispatch(new maskActions.RequestGetAllMasks());

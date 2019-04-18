3dcfcc47e0956ed25c87cdf0972224d88678b5a9
disney50 <36032319+disney50@users.noreply.github.com>
2019-04-18 22:11:26 +0200

    some players setup

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -4,8 +4,11 @@ import { AppState } from 'src/app/store/app.state';
 import { Store } from '@ngrx/store';
 import * as inventoryActions from '../../store/actions/inventory.actions';
 import * as inventorySelectors from '../../store/selectors/inventory.selectors';
+import * as playersActions from '../../store/actions/players.actions';
+import * as playersSelectors from '../../store/selectors/players.selectors';
 import { InventoryItem } from 'src/app/models/InventoryItem';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Player } from 'src/app/models/Player';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,13 +22,16 @@ export class DashboardComponent implements OnInit {
   tertiaryBtn = undefined;
 
   inventoryItems = [];
-
-  shouldShowList = false;
-  shouldShowDetail = false;
-  shouldShowAdd = false;
-
   selectedInventoryItem = {} as InventoryItem;
   newInventoryItem = {} as InventoryItem;
+  // shouldShowInventoryList = false;
+  // shouldShowInventoryDetail = false;
+  shouldShowInventoryAdd = false;
+
+  players = [];
+  selectedPlayer = {} as Player;
+  newPlayers = {} as Player;
+  shouldShowPlayersAdd = false;
 
   constructor(
     private router: Router,
@@ -48,39 +54,71 @@ export class DashboardComponent implements OnInit {
     this.tertiaryBtn = btn;
   }
 
-  toggleShowList() {
-    this.shouldShowList = true;
-    this.shouldShowDetail = false;
-    this.shouldShowAdd = false;
+  // toggleShowList() {
+  //   this.shouldShowInventoryList = true;
+  //   this.shouldShowInventoryDetail = false;
+  //   this.shouldShowInventoryAdd = false;
+  // }
+
+  // toggleShowAdd() {
+  //   this.shouldShowInventoryList = false;
+  //   this.shouldShowInventoryDetail = false;
+  //   this.shouldShowInventoryAdd = true;
+  // }
+
+  // toggleShowDetail() {
+  //   this.shouldShowInventoryList = false;
+  //   this.shouldShowInventoryDetail = true;
+  //   this.shouldShowInventoryAdd = false;
+  // }
+
+  displayPlayersList() {
+    this.secondaryBtn === 'U19' || this.secondaryBtn === 'U15' || this.secondaryBtn === 'U13'
+      ? this.store.dispatch(new playersActions.RequestGetPlayersByGender(this.tertiaryBtn))
+      : this.store.dispatch(new playersActions.RequestGetPlayersByAgeGroup(this.secondaryBtn));
   }
 
-  toggleShowAdd() {
-    this.shouldShowList = false;
-    this.shouldShowDetail = false;
-    this.shouldShowAdd = true;
+  displayPlayersAdd() {
+    this.shouldShowInventoryAdd = true;
   }
 
-  toggleShowDetail() {
-    this.shouldShowList = false;
-    this.shouldShowDetail = true;
-    this.shouldShowAdd = false;
+  clickAddPlayers() {
+    if (!this.newInventoryItem.type ||
+      !this.newInventoryItem.number ||
+      !this.newInventoryItem.brand ||
+      !this.newInventoryItem.color ||
+      !this.newInventoryItem.description ||
+      !this.newInventoryItem.status) {
+      alert('You forgot to fill in some fields');
+    } else {
+      this.addInventory();
+    }
   }
 
-  displayePlayerList() {
-    this.toggleShowList();
-    // this.secondaryBtn === 'U19'
-    //   ? this
+  addPlayers() {
+    this.angularFirestore.collection('/inventory/').ref.where('type', '==', this.newInventoryItem.type)
+      .where('number', '==', this.newInventoryItem.number).get().then(snapShot => {
+        if (snapShot.size === 0) {
+          this.angularFirestore.collection('inventory').add(this.newInventoryItem);
+          alert(this.newInventoryItem.type + ' added');
+        } else {
+          alert(this.newInventoryItem.type + ' number already exists');
+        }
+      });
+  }
+
+  displayPlayersDetail() {
+    alert('displayPlayersDetail');
   }
 
   displayInventoryList() {
-    this.toggleShowList();
     this.secondaryBtn === 'Status'
       ? this.store.dispatch(new inventoryActions.RequestGetInventoryItemsByStatus(this.tertiaryBtn))
       : this.store.dispatch(new inventoryActions.RequestGetInventoryItemsByType(this.tertiaryBtn));
   }
 
   displayInventoryAdd() {
-    this.toggleShowAdd();
+    this.shouldShowInventoryAdd = true;
   }
 
   clickAddInventory() {
@@ -109,7 +147,6 @@ export class DashboardComponent implements OnInit {
   }
 
   displayInventoryDetail(selectedInventoryItem: InventoryItem) {
-    this.toggleShowDetail();
     this.store.dispatch(new inventoryActions.GetSelectedInventoryItemSuccess(selectedInventoryItem));
   }
 

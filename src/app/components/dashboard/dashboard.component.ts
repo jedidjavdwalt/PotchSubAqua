3fd03d938ca453b83d01a8e267d4c85b33b9e48e
disney50 <36032319+disney50@users.noreply.github.com>
2019-04-24 09:29:01 +0200

    edit basic setup

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -43,11 +43,12 @@ export class DashboardComponent implements OnInit {
   shouldShowPlayersAdd = false;
 
   rentals = [];
-  selectedRental = null;
+  selectedRental = {} as Rental;
   newRental = {} as Rental;
   shouldShowRentalsList = false;
   shouldShowRentalsDetail = false;
   shouldShowRentalsAdd = false;
+  shouldShowRentalsEdit = false;
 
   availableMasks = [];
   availableSnorkels = [];
@@ -55,11 +56,15 @@ export class DashboardComponent implements OnInit {
   availableSticks = [];
   availableFins = [];
 
-  newRentalMask = undefined;
-  newRentalSnorkel = undefined;
-  newRentalGlove = undefined;
-  newRentalStick = undefined;
-  newRentalFins = undefined;
+  selectedRentalMask = undefined;
+  selectedRentalSnorkel = undefined;
+  selectedRentalGlove = undefined;
+  selectedRentalStick = undefined;
+  selectedRentalFins = undefined;
+
+  dateKitOut: string;
+  dateKitDue: string;
+  selectedDateKitIn: string;
 
   constructor(
     private router: Router,
@@ -89,13 +94,9 @@ export class DashboardComponent implements OnInit {
     this.shouldShowInventoryList = false;
     this.shouldShowRentalsList = false;
 
-    this.shouldShowPlayersDetail = false;
-    this.shouldShowInventoryDetail = false;
-    this.shouldShowRentalsDetail = false;
-
-    this.shouldShowPlayersAdd = false;
-    this.shouldShowInventoryAdd = false;
-    this.shouldShowRentalsAdd = false;
+    this.toggleDetailOff();
+    this.toggleAddOff();
+    this.toggleEditOff();
   }
 
   toggleAddOff() {
@@ -110,16 +111,24 @@ export class DashboardComponent implements OnInit {
     this.shouldShowRentalsDetail = false;
   }
 
+  toggleEditOff() {
+    this.shouldShowRentalsEdit = false;
+  }
+
   toggleShowPlayersList() {
     this.shouldShowPlayersList = true;
     this.shouldShowInventoryList = false;
     this.shouldShowRentalsList = false;
+
+    this.toggleEditOff();
   }
 
   toggleShowInventoryList() {
     this.shouldShowPlayersList = false;
     this.shouldShowInventoryList = true;
     this.shouldShowRentalsList = false;
+
+    this.toggleEditOff();
   }
 
   toggleShowRentalsList() {
@@ -134,6 +143,7 @@ export class DashboardComponent implements OnInit {
     this.shouldShowRentalsDetail = false;
 
     this.toggleAddOff();
+    this.toggleEditOff();
   }
 
   toggleShowInventoryDetail() {
@@ -142,6 +152,7 @@ export class DashboardComponent implements OnInit {
     this.shouldShowRentalsDetail = false;
 
     this.toggleAddOff();
+    this.toggleEditOff();
   }
 
   toggleShowRentalDetail() {
@@ -150,6 +161,7 @@ export class DashboardComponent implements OnInit {
     this.shouldShowRentalsDetail = true;
 
     this.toggleAddOff();
+    this.toggleEditOff();
   }
 
   toggleShowPlayersAdd() {
@@ -158,6 +170,7 @@ export class DashboardComponent implements OnInit {
     this.shouldShowRentalsAdd = false;
 
     this.toggleDetailOff();
+    this.toggleEditOff();
   }
 
   toggleShowInventoryAdd() {
@@ -166,6 +179,7 @@ export class DashboardComponent implements OnInit {
     this.shouldShowRentalsAdd = false;
 
     this.toggleDetailOff();
+    this.toggleEditOff();
   }
 
   toggleShowRentalsAdd() {
@@ -174,6 +188,14 @@ export class DashboardComponent implements OnInit {
     this.shouldShowRentalsAdd = true;
 
     this.toggleDetailOff();
+    this.toggleEditOff();
+  }
+
+  toggleShowRentalsEdit() {
+    this.shouldShowRentalsEdit = true;
+
+    this.toggleDetailOff();
+    this.toggleAddOff();
   }
 
   displayPlayersList() {
@@ -317,12 +339,16 @@ export class DashboardComponent implements OnInit {
   }
 
   displayRentalsAdd() {
+    this.newRental.inventoryItems = [];
+
     this.store.dispatch(new playersActions.RequestGetAllPlayers());
     this.store.dispatch(new inventoryActions.RequestGetAvailableMasks());
     this.store.dispatch(new inventoryActions.RequestGetAvailableSnorkels());
     this.store.dispatch(new inventoryActions.RequestGetAvailableGloves());
     this.store.dispatch(new inventoryActions.RequestGetAvailableSticks());
     this.store.dispatch(new inventoryActions.RequestGetAvailableFins());
+    this.newRental.inventoryItems = [];
+
     this.toggleShowRentalsAdd();
   }
 
@@ -330,39 +356,33 @@ export class DashboardComponent implements OnInit {
     if (!this.newRental.player ||
       !this.newRental.type) {
       alert('You forgot to select a player or type');
-    } else if (!this.newRentalMask &&
-      !this.newRentalSnorkel &&
-      !this.newRentalGlove &&
-      !this.newRentalStick &&
-      !this.newRentalFins) {
+    } else if (!this.selectedRentalMask &&
+      !this.selectedRentalSnorkel &&
+      !this.selectedRentalGlove &&
+      !this.selectedRentalStick &&
+      !this.selectedRentalFins) {
       alert('You forgot to select any inventory items');
     } else {
       this.newRental.dateKitOut = this.calculateDateKitOut();
       this.newRental.dateKitDue = this.calculateDateKitDue(this.newRental.type);
       this.newRental.feeDue = this.calculateFeeDue(this.newRental.type);
-      if (!this.newRental.feePaid) {
-        this.newRental.feePaid = 0;
+      if (this.selectedRentalMask) {
+        this.newRental.inventoryItems.push(this.selectedRentalMask);
       }
-      !this.newRental.feePaid
-        ? this.newRental.actionRequired = 'Player'
-        : this.newRental.actionRequired = 'None';
-      this.newRental.inventoryItems = [];
-      if (this.newRentalMask) {
-        this.newRental.inventoryItems.push(this.newRentalMask);
+      if (this.selectedRentalSnorkel) {
+        this.newRental.inventoryItems.push(this.selectedRentalSnorkel);
       }
-      if (this.newRentalSnorkel) {
-        this.newRental.inventoryItems.push(this.newRentalSnorkel);
+      if (this.selectedRentalGlove) {
+        this.newRental.inventoryItems.push(this.selectedRentalGlove);
       }
-      if (this.newRentalGlove) {
-        this.newRental.inventoryItems.push(this.newRentalGlove);
+      if (this.selectedRentalStick) {
+        this.newRental.inventoryItems.push(this.selectedRentalStick);
       }
-      if (this.newRentalStick) {
-        this.newRental.inventoryItems.push(this.newRentalStick);
-      }
-      if (this.newRentalFins) {
-        this.newRental.inventoryItems.push(this.newRentalFins);
+      if (this.selectedRentalFins) {
+        this.newRental.inventoryItems.push(this.selectedRentalFins);
       }
       this.newRental.id = this.calculateId(moment().format().slice(0, 10) + '_' + this.newRental.player);
+      this.newRental.actionRequired = this.calculateActionRequired(this.newRental);
       this.addRentals();
     }
   }
@@ -394,6 +414,38 @@ export class DashboardComponent implements OnInit {
         : feeDue = 500;
 
     return feeDue;
+  }
+
+  calculateActionRequired(rental: Rental) {
+    let actionRequired: string;
+    const currentDate = moment();
+    const dateKitDue = moment(rental.dateKitDue.toDate());
+
+    if (rental.type === 'Day') {
+      if (rental.feePaid !== rental.feeDue) {
+        actionRequired = 'Player';
+      } else if (!rental.dateKitIn && currentDate > dateKitDue) {
+        actionRequired = 'Player';
+      } else {
+        actionRequired = 'None';
+      }
+    } else {
+      if (rental.feePaid !== rental.feeDue) {
+        if (rental.dateKitIn) {
+          actionRequired = 'None';
+        } else {
+          actionRequired = 'Player';
+        }
+      } else if (!rental.dateKitIn && currentDate > dateKitDue) {
+        actionRequired = 'Player';
+      } else if (rental.dateKitIn && !rental.feeReturned) {
+        actionRequired = 'Admin';
+      } else {
+        actionRequired = 'None';
+      }
+    }
+
+    return actionRequired;
   }
 
   addRentals() {
@@ -434,6 +486,108 @@ export class DashboardComponent implements OnInit {
         }
       });
     });
+  }
+
+  displayRentalsEdit() {
+    this.newRental = new Rental(Object.assign({}, this.selectedRental));
+    // this.newRental = this.selectedRental;
+
+    this.newRental.inventoryItems.forEach(inventoryItem => {
+      inventoryItem.includes('Mask')
+        ? this.selectedRentalMask = inventoryItem
+        : inventoryItem.includes('Snorkel')
+          ? this.selectedRentalSnorkel = inventoryItem
+          : inventoryItem.includes('Glove')
+            ? this.selectedRentalGlove = inventoryItem
+            : inventoryItem.includes('Stick')
+              ? this.selectedRentalStick = inventoryItem
+              : inventoryItem.includes('Fins')
+                ? this.selectedRentalFins = inventoryItem
+                : alert('Inventory item does not contain known type');
+    });
+
+    this.newRental.inventoryItems = [];
+
+    this.dateKitOut = this.newRental.dateKitOut.toDate().toString().slice(0, 15);
+    this.dateKitDue = this.newRental.dateKitDue.toDate().toString().slice(0, 15);
+
+    this.store.dispatch(new inventoryActions.RequestGetAvailableMasks());
+    this.store.dispatch(new inventoryActions.RequestGetAvailableSnorkels());
+    this.store.dispatch(new inventoryActions.RequestGetAvailableGloves());
+    this.store.dispatch(new inventoryActions.RequestGetAvailableSticks());
+    this.store.dispatch(new inventoryActions.RequestGetAvailableFins());
+
+    this.toggleShowRentalsEdit();
+  }
+
+  clickRentalsSaveEdit() {
+    if (this.selectedRentalMask) {
+      this.newRental.inventoryItems.push(this.selectedRentalMask);
+    }
+    if (this.selectedRentalSnorkel) {
+      this.newRental.inventoryItems.push(this.selectedRentalSnorkel);
+    }
+    if (this.selectedRentalGlove) {
+      this.newRental.inventoryItems.push(this.selectedRentalGlove);
+    }
+    if (this.selectedRentalStick) {
+      this.newRental.inventoryItems.push(this.selectedRentalStick);
+    }
+    if (this.selectedRentalFins) {
+      this.newRental.inventoryItems.push(this.selectedRentalFins);
+    }
+    if (this.selectedDateKitIn) {
+      this.newRental.dateKitIn = this.convertDateStringToTimestamp(this.selectedDateKitIn);
+    }
+    this.newRental.actionRequired = this.calculateActionRequired(this.newRental);
+
+    this.rentalsSaveEdit();
+  }
+
+  rentalsSaveEdit() {
+    if (this.newRental === this.selectedRental) {
+      console.log('equals');
+
+    }
+    // this.angularFirestore.collection('/rentals/').doc(this.newRental.id).update(this.newRental);
+    // alert('Rental added');
+
+    // this.newRental.inventoryItems.forEach(inventoryItem => {
+    //   this.angularFirestore.collection('/inventory/', ref => ref.where('rentalId', '==', inventoryItem)).get().subscribe(snapShot => {
+    //     if (snapShot.size === 1) {
+    //       this.newInventoryItem = new InventoryItem(snapShot.docs[0].data() as InventoryItemData);
+    //       this.newInventoryItem.status = 'Rented';
+    //       this.angularFirestore.collection('/inventory/').doc(this.newInventoryItem.id).update(this.newInventoryItem.toData());
+    //       if (this.newInventoryItem.type === 'Mask') {
+    //         this.store.dispatch(new inventoryActions.RequestGetAvailableMasks());
+    //       } else if (this.newInventoryItem.type === 'Snorkel') {
+    //         this.store.dispatch(new inventoryActions.RequestGetAvailableSnorkels());
+    //       } else if (this.newInventoryItem.type === 'Glove') {
+    //         this.store.dispatch(new inventoryActions.RequestGetAvailableGloves());
+    //       } else if (this.newInventoryItem.type === 'Stick') {
+    //         this.store.dispatch(new inventoryActions.RequestGetAvailableSticks());
+    //       } else if (this.newInventoryItem.type === 'Fins') {
+    //         this.store.dispatch(new inventoryActions.RequestGetAvailableFins());
+    //       }
+    //       alert(this.newInventoryItem.rentalId + ' status updated');
+    //       this.newInventoryItem = {} as InventoryItem;
+    //     } else if (snapShot.size === 0) {
+    //       alert(inventoryItem + ' not found');
+    //       return;
+    //     } else if (snapShot.size < 1) {
+    //       alert('More than one ' + inventoryItem);
+    //     }
+    //   });
+    // });
+  }
+
+  clickRentalsCancelEdit() {
+    this.newRental = {} as Rental;
+
+    this.dateKitOut = null;
+    this.dateKitDue = null;
+
+    this.toggleShowRentalDetail();
   }
 
   sliceAppState() {

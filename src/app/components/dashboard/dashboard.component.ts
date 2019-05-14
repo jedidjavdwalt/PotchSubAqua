8c59effc48b31d9bf9ee3666c89ec6379dd2d8ac
disney50 <36032319+disney50@users.noreply.github.com>
2019-05-14 19:57:40 +0200

    dashboard html simplified

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -26,44 +26,46 @@ export class DashboardComponent implements OnInit {
   secondaryBtn = undefined;
   tertiaryBtn = undefined;
 
-  inventoryItems = [];
-  selectedInventoryItem = null;
-  inventoryItem: InventoryItem = {} as InventoryItem;
-  shouldShowInventoryList = false;
-  shouldShowInventoryDetail = false;
-  shouldShowInventoryAdd = false;
-
   players = [];
   selectedPlayer = null;
-  playerToAdd: Player = {} as Player;
-  selectedBirthDate: string;
-  shouldShowPlayersList = false;
-  shouldShowPlayersDetail = false;
-  shouldShowPlayersAdd = false;
+
+  inventory = [];
+  selectedInventoryItem = null;
 
   rentals = [];
   selectedRental = {} as Rental;
-  rental = {} as Rental;
-  shouldShowRentalsList = false;
-  shouldShowRentalsDetail = false;
-  shouldShowRentalsAdd = false;
-  shouldShowRentalsEdit = false;
-
   availableMasks = [];
   availableSnorkels = [];
   availableGloves = [];
   availableSticks = [];
   availableFins = [];
 
-  selectedRentalMask = undefined;
-  selectedRentalSnorkel = undefined;
-  selectedRentalGlove = undefined;
-  selectedRentalStick = undefined;
-  selectedRentalFins = undefined;
-
-  dateKitOut: string;
-  dateKitDue: string;
-  selectedDateKitIn: string;
+  buttons = [
+    {
+      id: 'Players', secondary: [
+        { id: 'Senior', tertiary: [{ id: 'Ladies' }, { id: 'Men' }] },
+        { id: 'U19', tertiary: [{ id: 'Ladies' }, { id: 'Men' }] },
+        { id: 'U15', tertiary: [{ id: 'Ladies' }, { id: 'Men' }] },
+        { id: 'U13', tertiary: [{ id: 'Ladies' }, { id: 'Men' }] },
+        { id: 'U10', tertiary: [{ id: 'Ladies' }, { id: 'Men' }] },
+        { id: 'Add' },
+      ]
+    },
+    {
+      id: 'Inventory Items', secondary: [
+        { id: 'Statues', tertiary: [{ id: 'Available' }, { id: 'Rented' }] },
+        { id: 'Inventory Item Type', tertiary: [{ id: 'Mask' }, { id: 'Snorkel' }, { id: 'Glove' }, { id: 'Stick' }, { id: 'Fins' }] },
+        { id: 'Add' },
+      ]
+    },
+    {
+      id: 'Rentals', secondary: [
+        { id: 'Action Required', tertiary: [{ id: 'Admin' }, { id: 'Player' }] },
+        { id: 'Rental Type', tertiary: [{ id: 'Day' }, { id: 'Beginner' }, { id: 'Season' }] },
+        { id: 'Add' },
+      ]
+    }
+  ];
 
   constructor(
     private store: Store<AppState>,
@@ -73,301 +75,84 @@ export class DashboardComponent implements OnInit {
     private rentalsService: RentalsService,
   ) { }
 
-  activatePrimaryBtn(btn: string) {
+  primaryClicked(btn: string) {
     this.primaryBtn = btn;
     this.secondaryBtn = undefined;
     this.tertiaryBtn = undefined;
-
-    this.toggleAllOff();
   }
 
-  activateSecondaryBtn(btn: string) {
+  secondaryClicked(btn: string) {
     this.secondaryBtn = btn;
     this.tertiaryBtn = undefined;
   }
 
-  activateTertiaryBtn(btn: string) {
+  tertiaryClicked(btn: string) {
     this.tertiaryBtn = btn;
   }
 
-  toggleAllOff() {
-    this.shouldShowPlayersList = false;
-    this.shouldShowInventoryList = false;
-    this.shouldShowRentalsList = false;
+  displayAdd() {
+    if (this.primaryBtn === 'Players') {
+      return 'Players';
+    }
 
-    this.toggleDetailOff();
-    this.toggleAddOff();
-    this.toggleEditOff();
+    if (this.primaryBtn === 'Inventory Items') {
+      return 'Inventory Items';
+    }
+
+    if (this.primaryBtn === 'Rentals') {
+      // this.store.dispatch(new playersActions.RequestGetAllPlayers());
+      // this.store.dispatch(new inventoryActions.RequestGetAvailableMasks());
+      // this.store.dispatch(new inventoryActions.RequestGetAvailableSnorkels());
+      // this.store.dispatch(new inventoryActions.RequestGetAvailableGloves());
+      // this.store.dispatch(new inventoryActions.RequestGetAvailableSticks());
+      // this.store.dispatch(new inventoryActions.RequestGetAvailableFins());
+
+      return 'Rentals';
+    }
   }
 
-  toggleAddOff() {
-    this.shouldShowPlayersAdd = false;
-    this.shouldShowInventoryAdd = false;
-    this.shouldShowRentalsAdd = false;
-  }
+  displayList() {
+    if (this.primaryBtn === 'Players') {
+      // !this.tertiaryBtn
+      //   ? this.store.dispatch(new playersActions.RequestGetPlayersByAgeGroup(this.secondaryBtn))
+      //   : this.store.dispatch(new playersActions.RequestGetPlayersByGender(this.tertiaryBtn, this.secondaryBtn));
 
-  toggleDetailOff() {
-    this.shouldShowPlayersDetail = false;
-    this.shouldShowInventoryDetail = false;
-    this.shouldShowRentalsDetail = false;
-  }
+      return 'Players';
+    }
 
-  toggleEditOff() {
-    this.shouldShowRentalsEdit = false;
-  }
+    if (this.primaryBtn === 'Inventory Items') {
+      // this.secondaryBtn === 'Status'
+      //   ? this.store.dispatch(new inventoryActions.RequestGetInventoryItemsByStatus(this.tertiaryBtn))
+      //   : this.store.dispatch(new inventoryActions.RequestGetInventoryItemsByType(this.tertiaryBtn));
 
-  toggleShowPlayersList() {
-    this.shouldShowPlayersList = true;
-    this.shouldShowInventoryList = false;
-    this.shouldShowRentalsList = false;
+      return 'Inventory Items';
+    }
 
-    this.toggleEditOff();
-  }
+    if (this.primaryBtn === 'Rentals') {
+      // this.secondaryBtn === 'Action Required'
+      //   ? this.store.dispatch(new rentalsActions.RequestGetRentalsByActionRequired(this.tertiaryBtn))
+      //   : this.store.dispatch(new rentalsActions.RequestGetRentalsByType(this.tertiaryBtn));
 
-  toggleShowInventoryList() {
-    this.shouldShowPlayersList = false;
-    this.shouldShowInventoryList = true;
-    this.shouldShowRentalsList = false;
-
-    this.toggleEditOff();
-  }
-
-  toggleShowRentalsList() {
-    this.shouldShowPlayersList = false;
-    this.shouldShowInventoryList = false;
-    this.shouldShowRentalsList = true;
-  }
-
-  toggleShowPlayersDetail() {
-    this.shouldShowPlayersDetail = true;
-    this.shouldShowInventoryDetail = false;
-    this.shouldShowRentalsDetail = false;
-
-    this.toggleAddOff();
-    this.toggleEditOff();
-  }
-
-  toggleShowInventoryDetail() {
-    this.shouldShowPlayersDetail = false;
-    this.shouldShowInventoryDetail = true;
-    this.shouldShowRentalsDetail = false;
-
-    this.toggleAddOff();
-    this.toggleEditOff();
-  }
-
-  toggleShowRentalDetail() {
-    this.shouldShowPlayersDetail = false;
-    this.shouldShowInventoryDetail = false;
-    this.shouldShowRentalsDetail = true;
-
-    this.toggleAddOff();
-    this.toggleEditOff();
-  }
-
-  toggleShowPlayersAdd() {
-    this.shouldShowPlayersAdd = true;
-    this.shouldShowInventoryAdd = false;
-    this.shouldShowRentalsAdd = false;
-
-    this.toggleDetailOff();
-    this.toggleEditOff();
-  }
-
-  toggleShowInventoryAdd() {
-    this.shouldShowPlayersAdd = false;
-    this.shouldShowInventoryAdd = true;
-    this.shouldShowRentalsAdd = false;
-
-    this.toggleDetailOff();
-    this.toggleEditOff();
-  }
-
-  toggleShowRentalsAdd() {
-    this.shouldShowPlayersAdd = false;
-    this.shouldShowInventoryAdd = false;
-    this.shouldShowRentalsAdd = true;
-
-    this.toggleDetailOff();
-    this.toggleEditOff();
-  }
-
-  toggleShowRentalsEdit() {
-    this.shouldShowRentalsEdit = true;
-
-    this.toggleDetailOff();
-    this.toggleAddOff();
-  }
-
-  displayPlayersList() {
-    !this.tertiaryBtn
-      ? this.store.dispatch(new playersActions.RequestGetPlayersByAgeGroup(this.secondaryBtn))
-      : this.store.dispatch(new playersActions.RequestGetPlayersByGender(this.tertiaryBtn, this.secondaryBtn));
-
-    this.toggleShowPlayersList();
+      return 'Rentals';
+    }
   }
 
   displayPlayersDetail(selectedPlayer: Player) {
     this.store.dispatch(new playersActions.GetSelectedPlayerSuccess(selectedPlayer));
-    this.toggleShowPlayersDetail();
-  }
-
-  displayPlayersAdd() {
-    this.toggleShowPlayersAdd();
-  }
-
-  displayInventoryList() {
-    this.secondaryBtn === 'Status'
-      ? this.store.dispatch(new inventoryActions.RequestGetInventoryItemsByStatus(this.tertiaryBtn))
-      : this.store.dispatch(new inventoryActions.RequestGetInventoryItemsByType(this.tertiaryBtn));
-
-    this.toggleShowInventoryList();
   }
 
   displayInventoryDetail(selectedInventoryItem: InventoryItem) {
     this.store.dispatch(new inventoryActions.GetSelectedInventoryItemSuccess(selectedInventoryItem));
-    this.toggleShowInventoryDetail();
-  }
-
-  displayInventoryAdd() {
-    this.toggleShowInventoryAdd();
-  }
-
-  clickAddInventory() {
-    alert(this.inventoryService.createInventoryItemToAdd(this.inventoryItem));
-    this.inventoryItem = {} as InventoryItem;
-  }
-
-  displayRentalsList() {
-    this.secondaryBtn === 'Action Required'
-      ? this.store.dispatch(new rentalsActions.RequestGetRentalsByActionRequired(this.tertiaryBtn))
-      : this.store.dispatch(new rentalsActions.RequestGetRentalsByType(this.tertiaryBtn));
-
-    this.toggleShowRentalsList();
   }
 
   displayRentalsDetail(selectedRental: Rental) {
     this.store.dispatch(new rentalsActions.GetSelectedRentalSuccess(selectedRental));
-    this.toggleShowRentalDetail();
   }
-
-  displayRentalsAdd() {
-    this.store.dispatch(new playersActions.RequestGetAllPlayers());
-    this.store.dispatch(new inventoryActions.RequestGetAvailableMasks());
-    this.store.dispatch(new inventoryActions.RequestGetAvailableSnorkels());
-    this.store.dispatch(new inventoryActions.RequestGetAvailableGloves());
-    this.store.dispatch(new inventoryActions.RequestGetAvailableSticks());
-    this.store.dispatch(new inventoryActions.RequestGetAvailableFins());
-
-    this.toggleShowRentalsAdd();
-  }
-
-  // displayRentalsEdit() {
-  //   this.rental = new Rental(Object.assign({}, this.selectedRental));
-  //   // this.newRental = this.selectedRental;
-
-  //   this.rental.inventoryItems.forEach(inventoryItem => {
-  //     inventoryItem.includes('Mask')
-  //       ? this.selectedRentalMask = inventoryItem
-  //       : inventoryItem.includes('Snorkel')
-  //         ? this.selectedRentalSnorkel = inventoryItem
-  //         : inventoryItem.includes('Glove')
-  //           ? this.selectedRentalGlove = inventoryItem
-  //           : inventoryItem.includes('Stick')
-  //             ? this.selectedRentalStick = inventoryItem
-  //             : inventoryItem.includes('Fins')
-  //               ? this.selectedRentalFins = inventoryItem
-  //               : alert('Inventory item does not contain known type');
-  //   });
-
-  //   this.rental.inventoryItems = [];
-
-  //   this.dateKitOut = this.rental.startDate.toDate().toString().slice(0, 15);
-  //   this.dateKitDue = this.rental.dueDate.toDate().toString().slice(0, 15);
-
-  //   this.store.dispatch(new inventoryActions.RequestGetAvailableMasks());
-  //   this.store.dispatch(new inventoryActions.RequestGetAvailableSnorkels());
-  //   this.store.dispatch(new inventoryActions.RequestGetAvailableGloves());
-  //   this.store.dispatch(new inventoryActions.RequestGetAvailableSticks());
-  //   this.store.dispatch(new inventoryActions.RequestGetAvailableFins());
-
-  //   this.toggleShowRentalsEdit();
-  // }
-
-  // clickRentalsSaveEdit() {
-  //   if (this.selectedRentalMask) {
-  //     this.rental.inventoryItems.push(this.selectedRentalMask);
-  //   }
-  //   if (this.selectedRentalSnorkel) {
-  //     this.rental.inventoryItems.push(this.selectedRentalSnorkel);
-  //   }
-  //   if (this.selectedRentalGlove) {
-  //     this.rental.inventoryItems.push(this.selectedRentalGlove);
-  //   }
-  //   if (this.selectedRentalStick) {
-  //     this.rental.inventoryItems.push(this.selectedRentalStick);
-  //   }
-  //   if (this.selectedRentalFins) {
-  //     this.rental.inventoryItems.push(this.selectedRentalFins);
-  //   }
-  //   if (this.selectedDateKitIn) {
-  //     this.rental.endDate = this.convertDateStringToTimestamp(this.selectedDateKitIn);
-  //   }
-  //   this.rental.actionRequired = this.calculateActionRequired(this.rental);
-
-  //   this.rentalsSaveEdit();
-  // }
-
-  // rentalsSaveEdit() {
-  //   if (this.rental === this.selectedRental) {
-  //     console.log('equals');
-
-  //   }
-    // this.angularFirestore.collection('/rentals/').doc(this.newRental.id).update(this.newRental);
-    // alert('Rental added');
-
-    // this.newRental.inventoryItems.forEach(inventoryItem => {
-    //   this.angularFirestore.collection('/inventory/', ref => ref.where('rentalId', '==', inventoryItem)).get().subscribe(snapShot => {
-    //     if (snapShot.size === 1) {
-    //       this.newInventoryItem = new InventoryItem(snapShot.docs[0].data() as InventoryItemData);
-    //       this.newInventoryItem.status = 'Rented';
-    //       this.angularFirestore.collection('/inventory/').doc(this.newInventoryItem.id).update(this.newInventoryItem.toData());
-    //       if (this.newInventoryItem.type === 'Mask') {
-    //         this.store.dispatch(new inventoryActions.RequestGetAvailableMasks());
-    //       } else if (this.newInventoryItem.type === 'Snorkel') {
-    //         this.store.dispatch(new inventoryActions.RequestGetAvailableSnorkels());
-    //       } else if (this.newInventoryItem.type === 'Glove') {
-    //         this.store.dispatch(new inventoryActions.RequestGetAvailableGloves());
-    //       } else if (this.newInventoryItem.type === 'Stick') {
-    //         this.store.dispatch(new inventoryActions.RequestGetAvailableSticks());
-    //       } else if (this.newInventoryItem.type === 'Fins') {
-    //         this.store.dispatch(new inventoryActions.RequestGetAvailableFins());
-    //       }
-    //       alert(this.newInventoryItem.rentalId + ' status updated');
-    //       this.newInventoryItem = {} as InventoryItem;
-    //     } else if (snapShot.size === 0) {
-    //       alert(inventoryItem + ' not found');
-    //       return;
-    //     } else if (snapShot.size < 1) {
-    //       alert('More than one ' + inventoryItem);
-    //     }
-    //   });
-    // });
-  // }
-
-  // clickRentalsCancelEdit() {
-  //   this.rental = {} as Rental;
-
-  //   this.dateKitOut = null;
-  //   this.dateKitDue = null;
-
-  //   this.toggleShowRentalDetail();
-  // }
 
   sliceAppState() {
     this.store.select(inventorySelectors.inventoryItems).subscribe(inventoryItems => {
-      this.inventoryItems = inventoryItems;
-      this.inventoryItems.sort((a, b) => (a.number > b.number) ? 1 : -1);
+      this.inventory = inventoryItems;
+      this.inventory.sort((a, b) => (a.number > b.number) ? 1 : -1);
     });
 
     this.store.select(inventorySelectors.selectedInventoryItem).subscribe(selectedInventoryItem => {

b62792eb62a30fab81819cfbebdfdfa55c282ea1
disney50 <36032319+disney50@users.noreply.github.com>
2019-07-13 19:30:49 +0200

    environment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDZM9vo4cRkXuygzROkDJF8eZ8Ic1BwkkM',
+    authDomain: 'potchsubaqua.firebaseapp.com',
+    databaseURL: 'https://potchsubaqua.firebaseio.com',
+    projectId: 'potchsubaqua',
+    storageBucket: 'potchsubaqua.appspot.com',
+    messagingSenderId: '188805584571'
+  },
 };
